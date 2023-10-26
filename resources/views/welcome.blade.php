a853c99044dcf639ab33686ff48b84126e086640
William Bierhals <willambierhals>
2023-10-25 21:23:20 -0300

    :sparkles: Create Home/Events interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,28 @@
 <!-- Inclui o conteúdo do layout -->
 @section('content')
 
-<h1>home</h1>
-<img src="/img/banner.jpg" alt="Imagem Banner">
-
-@foreach($events as $event)
-    <p>{{ $event->title }} -- {{ $event->description }} -- {{ $event->city }}</p>
-@endforeach
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Pesquisar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+                <div class="card-body">
+                    <p class="card-date">01/11/2023</p>
+                    <h5 class="card-title">{{ $event->title }}</h5>
+                    <p class="card-participants">-- participantes</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
